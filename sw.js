732d4e8c8eebcbf830cdfddfc61ef9c768ba4b0f
wierts <wierts@gmail.com>
2026-03-20 16:59:40 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+
+const CACHE_NAME = 'sticky-checklist-v1774021340';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k.startsWith('sticky-checklist-v') && k!==CACHE_NAME).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(cached =>
+        cached || fetch(e.request).then(resp => {
+          const copy = resp.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+          return resp;
+        })
+      ).catch(() => caches.match('./index.html'))
+    );
+  }
+});
